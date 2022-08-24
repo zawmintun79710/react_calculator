8a3afec67dc4c3620560aef0d1810fedfa967cd0
zawmintun79710 <zawmintuncoe.mm@gmail.com>
2022-08-24 10:02:11 +0630

    fully commitment

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
-import React, { useState } from "react";
-import Wrapper from "./components/Wrapper";
-import Screen from "./components/Screen";
-import ButtonBox from "./components/ButtonBox";
-import Button from "./components/Button";
-import './App.css'
+import React,{useState} from "react";
+import { Textfit } from "react-textfit";
+import "./App.css"
 
 const btnValues = [
   {id:"clear",value:'AC'},{id:'addminus',value:"+-"},{id:'percent',value:"%"},{id:'divide',value:'/'},
-  {id:'seven',value:7}, {id:'eight',value:8}, {id:'nine',value:9}, {id:'multiply',value:"x"},
+  {id:'seven',value:7}, {id:'eight',value:8}, {id:'nine',value:9}, {id:'multiply',value:"*"},
   {id:'four',value:4}, {id:'five',value:5}, {id:'six',value:6}, {id:'subtract',value:"-"},
   {id:'one',value:1}, {id:'two',value:2}, {id:'three',value:3}, {id:'add',value:"+"},
-  {id:'zero',value:0},{id:'decimal',value:"."}, {id:'equals',value:"="}
+  {id:"rotate", value:<i className="fa-solid fa-bring-forward"></i>},{id:'zero',value:0},{id:'decimal',value:"."}, {id:'equals',value:"="}
 ];
 
 const toLocaleString = (num) =>
@@ -19,11 +16,37 @@ const toLocaleString = (num) =>
 
 const removeSpaces = (num) => num.toString().replace(/\s/g, "");
 
+const Wrapper = ({ children }) => {
+  return <div className="wrapper">{children}</div>;
+};
+
+const Screen = ({ value, display }) => {
+    return (
+      <div>
+        <Textfit className="result" id='result' max={70}>{display}</Textfit>
+        <Textfit className="display" id='display' max={70}>{value}</Textfit>
+      </div>
+    );
+  };
+
+const ButtonBox = ({ children }) => {
+  return <div className="buttonBox">{children}</div>;
+};
+
+const Button = ({ className, value, id, onClick }) => {
+  return (
+    <button className={className} onClick={onClick} id={id}>
+      {value}
+    </button>
+  );
+};
+
+
 const App = () => {
   let [calc, setCalc] = useState({
     sign: "",
     num: 0,
-    res: 0,
+    result: 0,
   });
 let [display, setDisplay] = useState('');
 
@@ -31,8 +54,7 @@ let [display, setDisplay] = useState('');
     e.preventDefault();
     const value = e.target.innerHTML;
     
-    setDisplay(
-      display === "" && value === "0"
+    setDisplay(display === "" && value === "0"
             ? "0"
             : (display < 1 && value >= 0 && display.includes(".")) || display >=1
             ? display+value
@@ -53,7 +75,7 @@ let [display, setDisplay] = useState('');
             : removeSpaces(calc.num) % 1 === 0
             ? toLocaleString(Number(removeSpaces(calc.num + value)))
             : toLocaleString(removeSpaces(calc.num + value)),
-        res: !calc.sign ? 0 : calc.res,
+        result: !calc.sign ? 0 : calc.result,
       });    
   };
 
@@ -61,77 +83,66 @@ let [display, setDisplay] = useState('');
     e.preventDefault();
     const value = e.target.innerHTML;
 
-    setDisplay(!calc.num.toString().includes(".") ? calc.num + value : calc.num);
+  // setDisplay(!calc.num.toString().includes(value) ? calc.num + value: /[*/+-]/g.test(display) && !calc.num.toString().includes(value) ? display +calc.num+ value : calc.num);
+  setDisplay((calc.num === 0 && display === "") ? calc.num + value : calc.num.toString().includes(value) ? calc.num : display + value)
+    
     setCalc({
       ...calc,
-      num: !calc.num.toString().includes(".") ? calc.num + value : calc.num,
+      num: (calc.num === 0 && display === "") ? calc.num + value : calc.num.toString().includes(value) ? calc.num : display + value,
     });
   };
 
-  const signClickHandler = (e) => {
+  const operatorClickHandler = (e) => {
     e.preventDefault();
     const value = e.target.innerHTML;
     
-    setDisplay(display.endsWith(value)? display: display + value);
+    function replaceOp(display){
+      let array = display.split("")
+      let result = array.splice(array.length-1,1,value)
+      return array.join("")
+      }
+
     setCalc({
       ...calc,
-      sign: value,
-      res: !calc.res && calc.num ? calc.num : calc.res,
+      sign: display === "" ? (calc.num + value).slice(0,1) : value,
+      result: !calc.result && calc.num ? calc.num : calc.result,
       num: 0,
     });
+
+  setDisplay(display.includes("=") ? calc.result + value :display === "" ? calc.num + value :display.endsWith(display[display.length-1].match(/[*/+-]/g))? replaceOp(display): display + value);
   };
 
   const equalsClickHandler = (e) => {
     
-   
-    if (calc.sign && calc.num) {
-
-      const math = (a,b,sign) =>
-        sign === "x"
-          ? a * b 
-          : sign === "/" 
-          ? a / b
-          : sign === "+"
-          ? a + b
-          : a - b     
+    if (calc.sign && calc.num) {             
       
       setCalc({
         ...calc,
-        res: 
-          calc.num === 0 && calc.sign === "/"
-            ? "Can't divide with 0"
-            : toLocaleString(
-                math(
-                  Number(calc.res),
-                  Number(calc.num),
-                  calc.sign                  
-      )
-      ),
-      
+        result: Function("return "+display)(),
         sign: "",
         num: 0,               
   });
   setDisplay(display+e.target.innerHTML);
-  }
+}
 }
 
   const invertClickHandler = () => {
     setCalc({
       ...calc,
       num: calc.num ? toLocaleString(removeSpaces(calc.num) * -1) : 0,
-      res: calc.res ? toLocaleString(removeSpaces(calc.res) * -1) : 0,
+      result: calc.result ? toLocaleString(removeSpaces(calc.result) * -1) : 0,
       sign: "",
     });
   };
 
   const percentClickHandler = () => {
     let num = calc.num ? parseFloat(removeSpaces(calc.num)) : 0;
-    let res = calc.res ? parseFloat(removeSpaces(calc.res)) : 0;
+    let result = calc.result ? parseFloat(removeSpaces(calc.result)) : 0;
 
     setCalc({
       ...calc,
       num: (num /= Math.pow(100, 1)),
-      res: (res /= Math.pow(100, 1)),
+      result: (result /= Math.pow(100, 1)),
       sign: "",
     });
   };
@@ -142,13 +153,13 @@ let [display, setDisplay] = useState('');
       ...calc,
       sign: "",
       num: 0,
-      res: 0,
+      result: 0,
     });
   };
 
   return (
     <Wrapper>
-      <Screen value={calc.num ? calc.num : calc.sign ? calc.sign : calc.res} display={display} />
+      <Screen value={calc.num ? calc.num : calc.sign ? calc.sign : calc.result} display={display} />
       <ButtonBox>
         {btnValues.map((btn,index) => {
           return (
@@ -165,8 +176,8 @@ let [display, setDisplay] = useState('');
                   ? percentClickHandler
                   : btn.value === "="
                   ? equalsClickHandler
-                  : btn.value === "/" || btn.value === "x" || btn.value === "-" || btn.value === "+"
-                  ? signClickHandler
+                  : btn.value === "/" || btn.value === "*" || btn.value === "-" || btn.value === "+"
+                  ? operatorClickHandler
                   : btn.value === "."
                   ? decimalClickHandler
                   : numClickHandler
